c8e522efe03d6fbb14faf0c2de1fd0a2e75479b6
John Rafael V. Gonzales <johnrafael.gonzales@neu.edu.ph>
2026-03-12 02:30:05 +0000

    remove the log in section

--- src/firebase/client-provider.tsx
+++ src/firebase/client-provider.tsx
@@ -1,12 +1,7 @@
 'use client';
 import { type PropsWithChildren } from 'react';
 import { FirebaseProvider } from './provider';
-import { UserProvider } from './auth/use-user';
 
 export function FirebaseClientProvider({ children }: PropsWithChildren) {
-  return (
-    <FirebaseProvider>
-      <UserProvider>{children}</UserProvider>
-    </FirebaseProvider>
-  );
+  return <FirebaseProvider>{children}</FirebaseProvider>;
 }
--- src/firebase/index.ts
+++ src/firebase/index.ts
@@ -45,6 +45,5 @@ export {
   useAuth,
 } from './provider';
 export { FirebaseClientProvider } from './client-provider';
-export { useUser } from './auth/use-user';
 export { useCollection } from './firestore/use-collection';
 export { useDoc } from './firestore/use-doc';
